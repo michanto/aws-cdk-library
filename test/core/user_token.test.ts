bd391cbcdf933d1bd319e2079d96442c8f8e1554
Michael Antonio <michanto@amazon.com>
2024-03-31 07:15:20 -0700

    Show different resolutions in different stacks.
    Better demonstration.

--- test/core/user_token.test.ts
+++ test/core/user_token.test.ts
@@ -1,49 +1,81 @@
 import { App, CustomResource, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Code, Function, IFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Bucket, IBucket } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
 import { AppToken, Singleton, StackToken } from '../../src';
+
+export class BadFunction extends Function {
+  constructor(scope: Construct, id: string ) {
+    super(scope, id, {
+      code: Code.fromInline('bad code'),
+      handler: 'handler',
+      runtime: Runtime.NODEJS_18_X,
+    });
+  }
+}
+
+export class StackTokenCustomResource extends CustomResource {
+  constructor(scope: Construct, id: string, fn: IFunction ) {
+    super(scope, id, {
+      serviceToken: fn.functionArn,
+      properties: {
+        bucketName: StackToken.string(scope, 'bucketName'),
+        pojo: StackToken.any(scope, 'pojo'),
+        list: StackToken.list(scope, 'list'),
+        aNumber: StackToken.number(scope, 'aNumber'),
+      },
+      pascalCaseProperties: true,
+    });
+  }
+}
+
+export class StackTokenStack extends Stack {
+  readonly bucket: IBucket;
+  readonly handler: IFunction;
+  constructor(scope: Construct, id: string ) {
+    super(scope, id);
+    this.bucket = new Bucket(this, 'Bucket', {
+      bucketName: StackToken.string(this, 'bucketName'),
+    });
+
+    this.handler = Singleton.create(this, 'Function', (s, i) => new BadFunction(s, i)) as Function;
+    new StackTokenCustomResource(this, 'CustomResource', this.handler);
+  }
+}
 
 describe('UserToken tests', () => {
   test('StackToken happy path.', () => {
     // GIVEN
-    const stack = new Stack();
-    let bucket = new Bucket(stack, 'Bucket', {
-      bucketName: StackToken.string(stack, 'bucketName'),
-    });
+    const app = new App();
+    const stackA = new StackTokenStack(app, 'StackA');
+    const stackB = new StackTokenStack(app, 'StackB');
 
-    let fn = Singleton.create(stack, 'Function', (s, id) => new Function(s, id, {
-      code: Code.fromInline('bad code'),
-      handler: 'handler',
-      runtime: Runtime.NODEJS_18_X,
-    })) as Function;
-
-    new CustomResource(stack, 'CustomResource', {
-      serviceToken: fn.functionArn,
-      properties: {
-        bucketName: StackToken.string(bucket, 'bucketName'),
-        pojo: StackToken.any(bucket, 'pojo'),
-        list: StackToken.list(bucket, 'list'),
-        aNumber: StackToken.number(bucket, 'aNumber'),
-      },
-      pascalCaseProperties: true,
-    });
-
-    StackToken.resolveString(stack, 'bucketName', { produce: () => 'my_bucket' });
-    StackToken.resolveAny(stack, 'pojo', {
+    StackToken.resolveString(stackA, 'bucketName', { produce: () => 'my_bucket' });
+    StackToken.resolveString(stackB, 'bucketName', { produce: () => 'other_bucket' });
+    StackToken.resolveAny(stackA, 'pojo', {
       produce: () => {
         // Note:  does not work without the 'return'.
         return { value: 'any' };
       },
     });
-    StackToken.resolveList(stack, 'list', { produce: () => ['one', 'two', 'three'] });
-    StackToken.resolveNumber(stack, 'aNumber', { produce: () => 10 });
+    StackToken.resolveAny(stackB, 'pojo', {
+      produce: () => {
+        // Note:  does not work without the 'return'.
+        return { value: 'none' };
+      },
+    });
+    StackToken.resolveList(stackA, 'list', { produce: () => ['one', 'two', 'three'] });
+    StackToken.resolveNumber(stackA, 'aNumber', { produce: () => 10 });
+    StackToken.resolveList(stackB, 'list', { produce: () => ['Manny', 'Moe', 'Mike'] });
+    StackToken.resolveNumber(stackB, 'aNumber', { produce: () => 100 });
 
     // WHEN
-    const template = Template.fromStack(stack);
+    const templateA = Template.fromStack(stackA);
+    const templateB = Template.fromStack(stackB);
 
     // THEN
-    template.templateMatches({
+    templateA.templateMatches({
       Resources: {
         Bucket83908E77: {
           Type: 'AWS::S3::Bucket',
@@ -57,6 +89,24 @@ describe('UserToken tests', () => {
             Pojo: { value: 'any' },
             List: ['one', 'two', 'three'],
             ANumber: 10,
+          },
+        },
+      },
+    });
+    templateB.templateMatches({
+      Resources: {
+        Bucket83908E77: {
+          Type: 'AWS::S3::Bucket',
+          Properties: {
+            BucketName: 'other_bucket',
+          },
+        },
+        CustomResource: {
+          Properties: {
+            BucketName: 'other_bucket',
+            Pojo: { value: 'none' },
+            List: ['Manny', 'Moe', 'Mike'],
+            ANumber: 100,
           },
         },
       },
