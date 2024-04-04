9b8c1e32254555426b574d8e13f6cb68dba137ab
Michael Antonio <michanto@amazon.com>
2024-04-04 09:14:06 -0700

    Expose the host from the Transform.
    Makes it easier for the user to tell what is going to happen.

--- src/transforms/cfn_transform.ts
+++ src/transforms/cfn_transform.ts
@@ -1,4 +1,4 @@
-import { Construct } from 'constructs';
+import { Construct, IConstruct } from 'constructs';
 import { ICfnTransform } from './icfn_transform';
 import { CFN_TRANSFORM_RTTI } from './private/transform_rtti';
 import { TransformHost } from './transform_host';
@@ -13,22 +13,28 @@ import { TransformHost } from './transform_host';
  */
 export abstract class CfnTransform extends Construct implements ICfnTransform {
   /**
-     * Returns `true` if a construct is a CfnTransform.
-     *
-     * Uses duck-typing instead of `instanceof` to allow CfnTransforms from different
-     * versions of this library to be included in the same stack.
-     *
-     * @returns The construct as a CfnTransform or undefined if it is not a CfnTransform.
-     */
+   * Returns `true` if a construct is a CfnTransform.
+   *
+   * Uses duck-typing instead of `instanceof` to allow CfnTransforms from different
+   * versions of this library to be included in the same stack.
+   *
+   * @returns The construct as a CfnTransform or undefined if it is not a CfnTransform.
+   */
   public static isCfnTransform(x: any): x is CfnTransform {
     return CFN_TRANSFORM_RTTI.hasRtti(x);
   }
+
+  /**
+   * Which construct will apply this transform.
+   */
+  readonly host: IConstruct;
 
   public constructor(scope: Construct, readonly id: string) {
     super(scope, id);
     // Calls TransformHost.hook on the host Stack and/or CfnElement to ensure there is a host for the Transform.
     TransformHost.ensureHosted(this);
     CFN_TRANSFORM_RTTI.addRtti(this);
+    this.host = TransformHost.of(this);
   }
 
   /**
